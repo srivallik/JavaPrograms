4521e9fe3bf52300a0c794281ff8da1061b6649c
$r!V@ll! <38324880+srivallik@users.noreply.github.com>
2020-12-31 09:07:41 -0800

    Update FinalDemo.java
    updated code directly in git

--- src/Test/FinalDemo.java
+++ src/Test/FinalDemo.java
@@ -3,9 +3,9 @@ package Test;
 public class FinalDemo {
 	static final double pi = 3.14;
 
-	/*
-	 * void Change() { pi=2.14; }
-	 */
+	
+	 void Change() { pi=2.14; }
+	 
 	public static void main(String[] args) {
 		System.out.println(FinalDemo.pi);
 	}
