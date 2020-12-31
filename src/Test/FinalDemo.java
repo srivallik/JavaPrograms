25c8bbdd14061407346354cd19605833a2f9197a
$r!V@ll! <38324880+srivallik@users.noreply.github.com>
2020-12-31 09:13:29 -0800

    Update FinalDemo.java
    revert back to original

--- src/Test/FinalDemo.java
+++ src/Test/FinalDemo.java
@@ -4,7 +4,7 @@ public class FinalDemo {
 	static final double pi = 3.14;
 
 	
-	 void Change() { pi=2.14; }
+	 //void Change() { pi=2.14; }
 	 
 	public static void main(String[] args) {
 		System.out.println(FinalDemo.pi);
